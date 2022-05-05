a3dbe8bbff9f92a6b2a846e86cf746e8eb494330
bsh6463 <bsh9510@gmail.com>
2022-05-05 15:43:06 +0900

    220505 1. listView in listView

--- app/src/main/java/com/example/pathfinder/adapter/PathListViewAdapter.kt
+++ app/src/main/java/com/example/pathfinder/adapter/PathListViewAdapter.kt
@@ -1,34 +1,39 @@
 package com.example.pathfinder.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.TextView
 import com.example.pathfinder.R
 import com.example.pathfinder.model.json.Path
 import com.example.pathfinder.model.json.SubPath
 import org.w3c.dom.Text
+import kotlin.coroutines.coroutineContext
 
 class PathListViewAdapter(val list: MutableList<MutableList<Path>>): BaseAdapter() {
     var totalTime = 0
     var totalDistance = 0
     lateinit var subPathListViewAdapter: SubPathListViewAdapter
 
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
         var convertView = convertView
         val subPathListView = convertView?.findViewById<ListView>(R.id.subPath_listView)
+
 
         if(convertView == null){
             convertView = LayoutInflater.from(parent?.context).inflate(R.layout.listview_item, parent,false)
         }
 
 
-
         list[position].forEach {   path ->
             totalTime += path.totalTime!!
             totalDistance += path.totalDistance!!
+
             subPathListViewAdapter = SubPathListViewAdapter(path.subPathList as MutableList<SubPath>)
             subPathListView?.adapter = subPathListViewAdapter
         }
@@ -55,4 +60,6 @@ class PathListViewAdapter(val list: MutableList<MutableList<Path>>): BaseAdapter
     override fun getItemId(position: Int): Long {
         return position.toLong()
     }
+
+
 }
